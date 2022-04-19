97ab55856616b0bb59a64341897a16019e4ce953
MVP79 <vmircioi@yahoo.com>
2022-04-19 11:20:41 +0300

    Upload cod Java

---
+++ src/AsignmentOperator.java
@@ -0,0 +1,6 @@
+public class AsignmentOperator {
+    public static void main(String[] args) {
+        int variabila = 5;
+        System.out.println("Variabila are valoarea "+ variabila);
+    }
+}
